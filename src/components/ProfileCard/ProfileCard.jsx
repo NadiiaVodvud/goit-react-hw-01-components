1bb045f5f5a215589f1f6f11da34c6164b7ba292
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2023-02-01 01:58:41 +0200

    start

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+export const Profile = () => {
+  return <ProfileCard>{}</ProfileCard>;
+};
+
+Profile.PropTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.object({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export * from 'components/ProfileCard/ProfileCard.styled.jsx';
+export * from 'components/ProfileCard/ProfileCard.jsx';
